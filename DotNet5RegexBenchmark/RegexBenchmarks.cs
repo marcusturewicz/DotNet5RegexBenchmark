387a8466f4a7e5200bd33bcfd0ad9d08bdd1f9f2
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-03-18 18:14:45 +1100

    Code

---
+++ DotNet5RegexBenchmark/RegexBenchmarks.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace DotNet5RegexBenchmark
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    [MemoryDiagnoser]
+    public class RegexBenchmarks
+    {
+        private string _data;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _data = File.ReadAllText("input-text.txt");
+        }
+
+        [Benchmark]
+        public void Email() => Regex.Matches(_data, @"[\w\.+-]+@[\w\.-]+\.[\w\.-]+", RegexOptions.Compiled);
+
+        [Benchmark]
+        public void URI() => Regex.Matches(_data, @"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?", RegexOptions.Compiled);
+
+        [Benchmark]
+        public void IP() => Regex.Matches(_data, @"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])", RegexOptions.Compiled);
+    }
+}
